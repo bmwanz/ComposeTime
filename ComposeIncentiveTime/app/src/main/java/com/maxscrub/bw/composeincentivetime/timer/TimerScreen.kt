05e14054c973744e52361484a0bdabf4cf787075
BW <bmwanz@live.com>
2022-03-01 21:35:43 -0800

    Initial Commit

---
+++ ComposeIncentiveTime/app/src/main/java/com/maxscrub/bw/composeincentivetime/timer/TimerScreen.kt
@@ -0,0 +1,20 @@
+package com.maxscrub.bw.composeincentivetime.timer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TimerScreen() {
+
+}
+
+@Composable
+private fun ScreenContent() {
+
+}
+
+@Preview
+@Composable
+private fun ScreenContentPreview() {
+    ScreenContent()
+}
